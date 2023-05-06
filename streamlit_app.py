eaf3825ea193942f86c6bc0f78e0c2a8e6d7826a
Luciana <lnieto@ksu.edu>
2023-05-06 17:14:19 -0500

    fhh

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,34 +3,28 @@ import leafmap.foliumap as leafmap
 
 st.set_page_config(layout="wide")
 
-
-st.sidebar.title("Sobre Nosotros")
-st.sidebar.info(
-
-)
-
-st.sidebar.title("Quienes Somos ")
-st.sidebar.info(
-    """
-   Francisco Guerrero: 
-   Luciana Nieto: 
-   Santiago Calvin~o:    """
-)
-
-st.sidebar.title("Contactenos")
-st.sidebar.info(
-    """
-aca mail general de BC y/o AgriData
-    """
-)
+# Customize the sidebar
+markdown = """
+AGRIDATA x BuscoCampo
+"""
+st.sidebar.title("Sobre nosotros")
+st.sidebar.info(markdown)
 
 
-st.title("AGRIDATA X BuscoCampo")
+# Customize page title
+st.title("AgriData reportes")
 
 st.markdown(
     """
-
+    
     """
 )
+
+st.header("Instrucciones")
+
+markdown = """
+
+
+"""
 
 st.markdown(markdown)
